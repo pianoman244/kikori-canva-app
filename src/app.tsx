34f30d9902ef29236954b815f2245150c7b81f48
Nathan Chasse <nathan.chasse@unh.edu>
2024-11-10 16:52:53 -0500

    added lots of comments to app.tsx, getting back into development

--- src/app.tsx
+++ src/app.tsx
@@ -2,17 +2,17 @@ import { useState } from "react";
 import { Button, Rows, Text, Select, FormField, TextInput, Alert } from "@canva/app-ui-kit";
 import { FormattedMessage, useIntl } from "react-intl";
 import * as styles from "styles/components.css";
-import { selection, SelectionEvent, addPage, getCurrentPageContext, openDesign, addNativeElement } from "@canva/design";
+import { addPage, getCurrentPageContext, openDesign, addNativeElement } from "@canva/design";
 import { requestFontSelection, findFonts, getTemporaryUrl } from "@canva/asset";
-import { useAddElement } from "utils/use_add_element";
 import { useSelection } from "utils/use_selection_hook";
-import { upload } from "@canva/asset";
 
-import mongodb_response from "./api_calls/basic_call.json";
-import chatgpt_response from "./api_calls/chatgpt_demo.json";
+import mongodb_response from "./api_calls/basic_call.json"; // demo fetchActivity response if api is down
+import chatgpt_response from "./api_calls/chatgpt_demo.json"; // demo chatgpt response to avoid using tokens
 
 const API_URL = "https://kikori-slides-api-08c1d2c0a600.herokuapp.com";
 
+// helper function to call API to generate slides for given activity and grade level
+// activityId is just the activity id string, gradeLevel is something from the list ["PK-K", "1-2", "3-5", "MS", "HS"]
 async function generateSlides(activityId: string, gradeLevel: string) {
   const response = await fetch(API_URL + '/generateSlides', {
     method: 'POST',
@@ -30,30 +30,45 @@ async function generateSlides(activityId: string, gradeLevel: string) {
 }
 
 export const App = () => {
+  // Kikori's selected font for the slide titles
   const FONTS = {
     "Wedges": "YAEKEi3zNJo:0"
   };
 
-  const slideDeck = chatgpt_response["slides"][0];
+  const slideDeck = chatgpt_response["slides"][0]; // demo slide deck for testing purposes
 
-  const plaintextSelection = useSelection("plaintext");
+  /* state variables for various app elements */
+
+  const plaintextSelection = useSelection("plaintext"); // for tracking a user selection of plaintext for editing
   const plaintextElementSelected = plaintextSelection.count > 0;
-  const [slideSelected, setSlideSelected] = useState("");
-  const DEFAULT_ID = "6NyRnQ4h0mqQne3KE1Nm"
-  const [activityIdInput, setActivityIdInput] = useState(DEFAULT_ID);
-  const [slidesButtonDisabled, setSlidesButtonDisabled] = useState(true);
+  const currentSelection = useSelection("image"); // for tracking user selection of image
+  const isElementSelected = currentSelection.count > 0;
+  const [slideSelected, setSlideSelected] = useState(""); // for tracking which slide is selected for editing
+  const DEFAULT_ID = "6NyRnQ4h0mqQne3KE1Nm" // some activity ID from the LIVE database as a default
+  const [activityIdInput, setActivityIdInput] = useState(DEFAULT_ID); // for tracking activity ID input
+  const [slidesButtonDisabled, setSlidesButtonDisabled] = useState(true); // for enabling/disabling generate slides button
+
+  // for the "fetched slides" alert (pops up with Verify ID button is clicked)
   const [fetchAlertVisible, setFetchAlertVisible] = useState(false);
   const [fetchAlertMessage, setFetchAlertMessage] = useState("");
   const [fetchAlertTone, setFetchAlertTone] = useState("warn");
 
+  // for the "generating slides" alert (updates as ChatGPT generates slides and then new slides are added to the slide deck)
+  const [generatingAlertVisible, setGeneratingAlertVisible] = useState(false);
+  const [generatingAlertMessage, setGeneratingAlertMessage] = useState("Generating...");
+  const [generatingAlertTone, setGeneratingAlertTone] = useState("neutral");
+
+
+  /* handlers for app element interaction */ 
+
+  // runs whenever activity id text box is updated
   async function handleActivityIdInput(id) {
     setActivityIdInput(id);
     setSlidesButtonDisabled(true);
     setFetchAlertVisible(false);
   }
-
   
-  
+  // handler for "Verify activity ID" button
   async function handleFetchActivity() {
     const activityId = activityIdInput;
     const response = await fetch(API_URL + '/fetchActivity', {
@@ -80,41 +95,11 @@ export const App = () => {
 
   }
 
-
-
-  const addElement = useAddElement();
-
-  const onClick = () => {
-    addElement({
-      type: "text",
-      children: ["Hello world!"],
-    });
-  };
-
-  async function replacePlaintext() {
-    if (!plaintextElementSelected) {
-      return;
-    }
-
-    const draft = await plaintextSelection.read();
-    console.log(draft.contents);
-
-    for (const content of draft.contents) {
-      //const text = await extractText(mongodb_response["data"]["playing"]);
-      //console.log(text);
-      content.text = slideDeck[slideSelected][0];
-    }
-    await draft.save();
-  }
-
-  const handleSlideSelect = (selectionEvent) => {
-    setSlideSelected(selectionEvent);
-    console.log("selected slide: " + selectionEvent);
-  }
-
+  // used to draw the white rectangle containing text on content slides. uses Canva's weird shape drawing syntax
   const createRoundedRectPath = (width: number, height: number, radius: number) =>
     `M ${radius} 0 H ${width - radius} A ${radius} ${radius} 0 0 1 ${width} ${radius} V ${height - radius} A ${radius} ${radius} 0 0 1 ${width - radius} ${height} H ${radius} A ${radius} ${radius} 0 0 1 0 ${height - radius} V ${radius} A ${radius} ${radius} 0 0 1 ${radius} 0 Z`;
 
+  // helper function to add a Kikori-formatted content slide with input title and content
   async function addPageHelper(title: string, content: string) {
     const context = await getCurrentPageContext();
     const height = context.dimensions.height;
@@ -133,7 +118,7 @@ export const App = () => {
 
     const tm_top = 93;
 
-    const logo_ref = "MAGSXRuIsBQ:1";
+    const logo_ref = "MAGSXRuIsBQ:1"; // reference to an SVG of the Kikori logo (to put in the top left)
 
     addPage({
       "elements": [
@@ -199,10 +184,7 @@ export const App = () => {
     })
   }
 
-  const [generatingAlertVisible, setGeneratingAlertVisible] = useState(false);
-  const [generatingAlertMessage, setGeneratingAlertMessage] = useState("Generating...");
-  const [generatingAlertTone, setGeneratingAlertTone] = useState("neutral");
-
+  // handler for "Generate Slides" button
   async function generateSlidesButton() {
     setGeneratingAlertVisible(true);
     setGeneratingAlertMessage("Generating...");
@@ -227,16 +209,19 @@ export const App = () => {
     setGeneratingAlertTone("positive");
   }
 
+  // helper function for when I needed to figure out the reference for the fonts we use
   async function fontPickerButton() {
     const fontResponse = await requestFontSelection();
     console.log(fontResponse);
   }
 
+  // helper function for exploring beta method openDesign in Canva SDK
   async function handleOpenDesign(draft, helpers) {
     console.log(draft);
     console.log(helpers);
   }
 
+  // helper function to print all elements on the page to explore the elementBuilder thing in Canva SDK
   async function printElementsButton() {
     openDesign({ type: "current_page" }, async (draft, { elementBuilder }) => {
       draft.page.elements.forEach((element, index) => {
@@ -273,11 +258,7 @@ export const App = () => {
     });
   }
 
-  const intl = useIntl();
-
-  const currentSelection = useSelection("image");
-  const isElementSelected = currentSelection.count > 0;
-
+  // event handler that can print the element that was just clicked and some details about it
   async function handleClick() {
     if (!isElementSelected) {
       console.log("no element selected");
@@ -295,6 +276,7 @@ export const App = () => {
     }
   }
 
+  // default generateSlides handler; used for testing the generateSlides logic reliably
   async function handleGenerateSlides() {
     const activityId = "6NyRnQ4h0mqQne3KE1Nm";
     const gradeLevel = "1-2";
@@ -350,7 +332,7 @@ export const App = () => {
         <Alert tone="info">  
           If you interact with Canva while slides are generating, they may generate out of order.
         </Alert>
-        {/*
+        {/* some helpful stuff I used when testing
         <Button variant="secondary" onClick={handleGenerateSlides} stretch>
           Generate instructions
         </Button>
@@ -359,13 +341,6 @@ export const App = () => {
         </Button>
         <Button variant="secondary" onClick={handleClick} stretch>
           print selected image ref
-        </Button>
-        <Button variant="tertiary" onClick={onClick} stretch>
-          {intl.formatMessage({
-            defaultMessage: "Do something cool",
-            description:
-              "Button text to do something cool. Creates a new text element when pressed.",
-          })}
         </Button>
         */}
       </Rows>
